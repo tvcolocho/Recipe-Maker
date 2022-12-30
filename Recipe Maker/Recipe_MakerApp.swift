cf81d411920ffa93cd2959cbf6e9a1584821d39b
tvcolocho <tvcolocho@gmail.com>
2022-12-30 14:43:26 -0800

    Initial Commit

---
+++ Recipe Maker/Recipe_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_MakerApp.swift
+//  Recipe Maker
+//
+//  Created by Tatiana Colocho on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
